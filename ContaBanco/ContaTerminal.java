7ad3029881d45ae294abe139054793285ef88a94
dosanjosromano <dosanjosromano@gmail.com>
2024-08-01 10:38:42 -0300

    Implementa inputs e associa a variavéis.

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String msgNomeCliente = "Por favor, insira o seu Nome:";
+        String msgAgencia = "Por favor, insira o número da Agência";
+        String msgConta = "Por favor, insira o número da Conta:";
+        String msgSaldo = "Por favor, insira o seu Saldo:";
+
+        System.out.println(msgNomeCliente);
+        String nomeCliente = scanner.nextLine();
+        System.out.println(msgAgencia);
+        String agencia = scanner.nextLine();
+        System.out.println(msgConta);
+        int conta = scanner.nextInt();
+        System.out.println(msgSaldo);
+        double saldo = scanner.nextDouble();
     }
 }
